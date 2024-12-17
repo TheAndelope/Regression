f3ca832af0e27b7513825e0f298a7d8f67a5466f
TheAndelope <164644527+TheAndelope@users.noreply.github.com>
2024-12-17 13:58:21 -0500

    API Integrated

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import dotenv
+import os
 import keras
+import matplotlib
+from polygon import RESTClient
 
+dotenv.load_dotenv()
+POLYGON_API_KEY = os.getenv("API_KEY")
+ticker = input("What Ticker Would You Like Predicted? ")
+client = RESTClient(POLYGON_API_KEY)
+
+aggs = []
+for a in client.list_aggs(
+    ticker,
+    1,
+    "minute",
+    "2022-01-01",
+    "2023-02-03",
+    limit=50000,
+):
+    aggs.append(a)
+
+print(aggs)
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(1,1)),            # Input layer
+    keras.layers.Dense(32, activation='relu'),  # Hidden layer with 32 neurons and ReLU activation
+    keras.layers.Dense(1),                      # Output layer with a single neuron (for regression)
+    keras.layers.Dropout(0.20)
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error')
